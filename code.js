15327adb8db8768cdede551b51e6fa2e48824867
LukeHoese <157862417+LukeHoese@users.noreply.github.com>
2024-11-29 20:07:52 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,7 +1,7 @@
 function convertToAdjList(adjMatrix) {
     adjList = [];
-    if (adjMatrix == []) {
-        return adjList;
+    if (!adjMatrix || adjMatrix.length === 0) {
+        return [];
     }
     for (let i = 0; i < adjMatrix.length; i++) {
         for (let j = 0; j < adjMatrix[j].length; j++) {
